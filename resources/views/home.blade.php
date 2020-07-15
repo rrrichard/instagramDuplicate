7988a4b97ac234cbe845569d424ac0ac73191a6b
Richard Rabulan <richarduuu@richards-mbp.home>
2020-07-15 22:24:00 +0100

    initial frontend done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,15 +10,42 @@
         </div>
         <div class="col-9 pt-5">
             <div>
-                <h1>Playstation Network</h1>
+                <h1>PlayStation Network</h1>
             </div>
             <div class="d-flex">
-                <div class="pr-5">153 posts</div>
-                <div class="pr-5">23k followers</div>
-                <div class="pr-5">212 following</div>
+                <div class="pr-5">
+                    <strong>153</strong> posts
+                </div>
+                <div class="pr-5">
+                    <strong>23k</strong> followers
+                </div>
+                <div class="pr-5">
+                    <strong>212</strong> following
+                </div>
             </div>
-            <div>
+            <div class="pt-4 font-weight-bold">
+                playstation.com
             </div>
+            <div class="">
+                PlayStation Network (PSN) is a digital media entertainment service provided by Sony Interactive Entertainment.
+            </div>
+            <div class="font-weight-bold">
+                <a href="https://www.playstation.com/en-gb/">www.playstation.com</a>
+            </div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="svg/lastofus.png" alt="lastofus"
+                class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="svg/deals.png" alt="hotdeals"
+                class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="svg/cyberpunk.png" alt="cyberpunk"
+                class="w-100">
         </div>
     </div>
 </div>
