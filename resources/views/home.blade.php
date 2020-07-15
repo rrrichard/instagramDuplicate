29814c729b71e5b8ffb2026f7cdeebde756274c8
Richard Rabulan <richarduuu@richards-mbp.home>
2020-07-15 19:43:26 +0100

    header css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,22 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="svg/playstation.png" alt="psnlogo"
+                style="max-height: 100px;"
+                class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>Playstation Network</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5">153 posts</div>
+                <div class="pr-5">23k followers</div>
+                <div class="pr-5">212 following</div>
+            </div>
+            <div>
             </div>
         </div>
     </div>
